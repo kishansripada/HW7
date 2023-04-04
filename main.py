901a4141e5f079850ace6bcc66c926666551fa5f
Kishan Sripada <kishansripada@gmail.com>
2023-04-04 15:55:08 +0000

    first few

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 # Your name: Kishan Sripada
 # Your student id: 5212 0336
 # Your email: ksripada@umich.edu
@@ -11,89 +10,119 @@ import os
 
 
 def read_data(filename):
-    full_path = os.path.join(os.path.dirname(__file__), filename)
-    f = open(full_path)
-    file_data = f.read()
-    f.close()
-    json_data = json.loads(file_data)
-    return json_data
+  full_path = os.path.join(os.path.dirname(__file__), filename)
+  f = open(full_path)
+  file_data = f.read()
+  f.close()
+  json_data = json.loads(file_data)
+  return json_data
+
 
 def open_database(db_name):
-    path = os.path.dirname(os.path.abspath(__file__))
-    conn = sqlite3.connect(path+'/'+db_name)
-    cur = conn.cursor()
-    return cur, conn
+  path = os.path.dirname(os.path.abspath(__file__))
+  conn = sqlite3.connect(path + '/' + db_name)
+  cur = conn.cursor()
+  return cur, conn
+
 
 def make_positions_table(data, cur, conn):
-    positions = []
-    for player in data['squad']:
-        position = player['position']
-        if position not in positions:
-            positions.append(position)
-    cur.execute("CREATE TABLE IF NOT EXISTS Positions (id INTEGER PRIMARY KEY, position TEXT UNIQUE)")
-    for i in range(len(positions)):
-        cur.execute("INSERT OR IGNORE INTO Positions (id, position) VALUES (?,?)",(i, positions[i]))
-    conn.commit()
+  positions = []
+  for player in data['squad']:
+    position = player['position']
+    if position not in positions:
+      positions.append(position)
+  cur.execute(
+    "CREATE TABLE IF NOT EXISTS Positions (id INTEGER PRIMARY KEY, position TEXT UNIQUE)"
+  )
+  for i in range(len(positions)):
+    cur.execute("INSERT OR IGNORE INTO Positions (id, position) VALUES (?,?)",
+                (i, positions[i]))
+  conn.commit()
+
 
 def make_players_table(data, cur, conn):
-    cur.execute("CREATE TABLE IF NOT EXISTS Players (id INTEGER PRIMARY KEY, name TEXT, position_id INTEGER, birthyear INTEGER, nationality TEXT)")
-    for player in data['squad']:
-        cur.execute("SELECT id FROM Positions WHERE position=?", (player['position'],))
-        position_id = cur.fetchone()[0]
-        birthyear = int(player['dateOfBirth'][:4])
-        cur.execute("INSERT OR IGNORE INTO Players (id, name, position_id, birthyear, nationality) VALUES (?,?,?,?,?)", (player['id'], player['name'], position_id, birthyear, player['nationality']))
-    conn.commit()
+  cur.execute(
+    "CREATE TABLE IF NOT EXISTS Players (id INTEGER PRIMARY KEY, name TEXT, position_id INTEGER, birthyear INTEGER, nationality TEXT)"
+  )
+  for player in data['squad']:
+    cur.execute("SELECT id FROM Positions WHERE position=?",
+                (player['position'], ))
+    position_id = cur.fetchone()[0]
+    birthyear = int(player['dateOfBirth'][:4])
+    cur.execute(
+      "INSERT OR IGNORE INTO Players (id, name, position_id, birthyear, nationality) VALUES (?,?,?,?,?)",
+      (player['id'], player['name'], position_id, birthyear,
+       player['nationality']))
+  conn.commit()
 
+
+def nationality_search(countries, cur, conn):
+  results = []
+  for country in countries:
+    cur.execute(
+      "SELECT name, position_id, nationality FROM Players WHERE nationality=?",
+      (country, ))
+    results.extend(cur.fetchall())
+  return results
+
+
+def birthyear_nationality_search(age, country, cur, conn):
+  birthyear_threshold = 2023 - age
+  cur.execute(
+    "SELECT name, nationality, birthyear FROM Players WHERE nationality=? AND birthyear<?",
+    (country, birthyear_threshold))
+  return cur.fetchall()
 
 
 class TestAllMethods(unittest.TestCase):
-    def setUp(self):
-        path = os.path.dirname(os.path.abspath(__file__))
-        self.conn = sqlite3.connect(path+'/'+'Football.db')
-        self.cur = self.conn.cursor()
-        self.conn2 = sqlite3.connect(path+'/'+'Football_seasons.db')
-        self.cur2 = self.conn2.cursor()
 
-    def test_players_table(self):
-        self.cur.execute('SELECT * from Players')
-        players_list = self.cur.fetchall()
+  def setUp(self):
+    path = os.path.dirname(os.path.abspath(__file__))
+    self.conn = sqlite3.connect(path + '/' + 'Football.db')
+    self.cur = self.conn.cursor()
+    self.conn2 = sqlite3.connect(path + '/' + 'Football_seasons.db')
+    self.cur2 = self.conn2.cursor()
 
-        self.assertEqual(len(players_list), 30)
-        self.assertEqual(len(players_list[0]),5)
-        self.assertIs(type(players_list[0][0]), int)
-        self.assertIs(type(players_list[0][1]), str)
-        self.assertIs(type(players_list[0][2]), int)
-        self.assertIs(type(players_list[0][3]), int)
-        self.assertIs(type(players_list[0][4]), str)
+  def test_players_table(self):
+    self.cur.execute('SELECT * from Players')
+    players_list = self.cur.fetchall()
 
-    def test_nationality_search(self):
-        x = sorted(nationality_search(['England'], self.cur, self.conn))
-        self.assertEqual(len(x), 11)
-        self.assertEqual(len(x[0]), 3)
-        self.assertEqual(x[0][0], "Aaron Wan-Bissaka")
+    self.assertEqual(len(players_list), 30)
+    self.assertEqual(len(players_list[0]), 5)
+    self.assertIs(type(players_list[0][0]), int)
+    self.assertIs(type(players_list[0][1]), str)
+    self.assertIs(type(players_list[0][2]), int)
+    self.assertIs(type(players_list[0][3]), int)
+    self.assertIs(type(players_list[0][4]), str)
 
-        y = sorted(nationality_search(['Brazil'], self.cur, self.conn))
-        self.assertEqual(len(y), 3)
-        self.assertEqual(y[2],('Fred', 2, 'Brazil'))
+  def test_nationality_search(self):
+    x = sorted(nationality_search(['England'], self.cur, self.conn))
+    self.assertEqual(len(x), 11)
+    self.assertEqual(len(x[0]), 3)
+    self.assertEqual(x[0][0], "Aaron Wan-Bissaka")
+
+    y = sorted(nationality_search(['Brazil'], self.cur, self.conn))
+    self.assertEqual(len(y), 3)
+    self.assertEqual(y[2], ('Fred', 2, 'Brazil'))
+
 
 def main():
 
-    #### FEEL FREE TO USE THIS SPACE TO TEST OUT YOUR FUNCTIONS
+  #### FEEL FREE TO USE THIS SPACE TO TEST OUT YOUR FUNCTIONS
 
-    json_data = read_data('football.json')
-    cur, conn = open_database('Football.db')
-    make_positions_table(json_data, cur, conn)
-    make_players_table(json_data, cur, conn)
-    conn.close()
+  json_data = read_data('football.json')
+  cur, conn = open_database('Football.db')
+  make_positions_table(json_data, cur, conn)
+  make_players_table(json_data, cur, conn)
+  conn.close()
 
-
-    seasons_json_data = read_data('football_PL.json')
-    cur2, conn2 = open_database('Football_seasons.db')
-    make_winners_table(seasons_json_data, cur2, conn2)
-    make_seasons_table(seasons_json_data, cur2, conn2)
-    conn2.close()
+  seasons_json_data = read_data('football_PL.json')
+  cur2, conn2 = open_database('Football_seasons.db')
+  make_winners_table(seasons_json_data, cur2, conn2)
+  make_seasons_table(seasons_json_data, cur2, conn2)
+  conn2.close()
 
 
 if __name__ == "__main__":
-    main()
-    unittest.main(verbosity = 2)
+  main()
+  unittest.main(verbosity=2)
